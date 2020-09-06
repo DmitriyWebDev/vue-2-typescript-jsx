f2e1eb743fbe75ff9b974fcc8cb09e2e84724c32
DmitriyWebDev <d-man_real_akkaunt.ru@mail.ru>
2020-09-06 23:01:20 +0600

    Added HelloWorld.tsx

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class HelloWorld extends Vue {
+  // Declare mounted lifecycle hook
+  mounted() {
+    console.log("mounted");
+  }
+
+  // Declare render function
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
